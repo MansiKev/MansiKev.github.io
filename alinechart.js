f0a333b352a21a7c379f4863225004f3211c23d9
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-06 10:11:13 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -36,9 +36,8 @@ var getScriptPromisify = (src) => {
             resultset.forEach(e=>{
                 var b={};
                 b["date"]= new Date(e["Order_Date"].id);
-                    b[e["@MeasureDimension"]["description"]]=e["@MeasureDimension"]["rawValue"];
-                }
-                // b[e["@MeasureDimension"]["description"]]=e["@MeasureDimension"]["rawValue"];
+                b[e["@MeasureDimension"]["description"]]=e["@MeasureDimension"]["rawValue"];
+    
                 data.push(b);
             })
             console.log(data);
