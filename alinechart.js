836e5e4f45557b339c9b6f50ea4a44606a618aa5
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-06 17:22:30 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -56,6 +56,7 @@ var getScriptPromisify = (src) => {
             var m={};
             for(var i=0;i<resultset.length;i++){
                 
+                
             }
             console.log(data);
             chart.data=data;
