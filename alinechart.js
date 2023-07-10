348df8239987331b6265dbb1bedccb7a9bcea6f9
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-10 11:02:02 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -83,10 +83,10 @@ var getScriptPromisify = (src) => {
                 var series = chart.series.push(new am4charts.LineSeries());
                 series.dataFields.valueY = measures[i];
                 series.dataFields.dateX = "date";
-                series.tooltipText = a[i].date;
+                series.tooltipText = {measures};
                 series.strokeWidth = 2;
                 series.minBulletDistance = 15;
-                console.log(a[i].date);
+                console.log(measures);
                 // Drop-shaped tooltips
                 series.tooltip.background.cornerRadius = 20;
                 series.tooltip.background.strokeOpacity = 0;
