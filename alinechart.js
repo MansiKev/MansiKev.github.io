c4d7d173211054e80353848f09215bfa738af05f
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-11 17:27:29 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -100,8 +100,8 @@ var getScriptPromisify = (src) => {
                 bullet.circle.radius = 4;
                 bullet.circle.fill = am4core.color("#fff");
 
-                chart.legend = new am4charts.Legend();
                 
+
                 var bullethover = bullet.states.create("hover");
                 bullethover.properties.scale = 1.3;
                 var series2 = chart.series.push(new am4charts.LineSeries());
@@ -110,7 +110,7 @@ var getScriptPromisify = (src) => {
                 series2.yAxis = valueAxis2;
             }
 
-
+            chart.legend = new am4charts.Legend();
             // Make a panning cursor
             chart.cursor = new am4charts.XYCursor();
             // chart.cursor.behavior = "panXY";
