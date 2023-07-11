2a9b51626bd2745266caa1c3535437abbf96c84a
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-11 11:51:57 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -109,8 +109,8 @@ var getScriptPromisify = (src) => {
             // Make a panning cursor
             chart.cursor = new am4charts.XYCursor();
             // chart.cursor.behavior = "panXY";
-            chart.cursor.xAxis = dateAxis;
-            chart.cursor.snapToSeries = series;
+            // chart.cursor.xAxis = dateAxis;
+            // chart.cursor.snapToSeries = series;
 
             // Create vertical scrollbar and place it before the value axis
             chart.scrollbarY = new am4core.Scrollbar();
