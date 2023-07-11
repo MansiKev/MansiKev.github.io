bd06865f7d1b62bde12f3c11df18ac4bc1643cc6
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-11 15:44:17 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -120,10 +120,10 @@ var getScriptPromisify = (src) => {
             chart.scrollbarY.parent = chart.leftAxesContainer;
             chart.scrollbarY.toBack();
 
-            // Create a horizontal scrollbar with previe and place it underneath the date axis
-            chart.scrollbarX = new am4charts.XYChartScrollbar();
-            chart.scrollbarX.series.push(series);
-            chart.scrollbarX.parent = chart.bottomAxesContainer;
+            // // Create a horizontal scrollbar with previe and place it underneath the date axis
+            // chart.scrollbarX = new am4charts.XYChartScrollbar();
+            // chart.scrollbarX.series.push(series);
+            // chart.scrollbarX.parent = chart.bottomAxesContainer;
 
             dateAxis.getSeriesDataItem = function (series, position) {
                 var key = this.axisFieldName + this.axisLetter;
