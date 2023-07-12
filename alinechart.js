04794b3e3d151488da2433f763d9ac0e30502cbf
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-12 11:26:28 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -75,7 +75,7 @@ var getScriptPromisify = (src) => {
             var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
             var valueAxis2 = chart.yAxes.push(new am4charts.ValueAxis());
             valueAxis2.renderer.opposite = true;
-            valueAxis2.syncWithAxis = valueAxis;
+            // valueAxis2.syncWithAxis = valueAxis;
             // Create series
             for (var i = 0; i < measures.length; i++) {
                 var series = chart.series.push(new am4charts.LineSeries());
