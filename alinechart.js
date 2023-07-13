c3d7353661ae9b043d4ba9ad29667688d86b3a3e
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-13 15:03:35 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -113,7 +113,7 @@ var firstapi = 1;
             chart.legend = new am4charts.Legend();
             chart.legend.position = "top";
             chart.legend.contentAlign = "right";
-            chart.legend.minHeight=80;
+            chart.legend.minHeight=50;
 
             // Make a panning cursor
             chart.cursor = new am4charts.XYCursor();
