f825a6ec7fa59c25c877582838e97d790a2ec11b
Mansi Kevadiya <mansikevadiya26@gmail.com>
2023-07-14 09:50:11 +0530

    a

--- alinechart.js
+++ alinechart.js
@@ -86,7 +86,8 @@ var firstapi = 1;
                 series.name = measure_description[i].split("$")[0];
                 series.stroke=measure_description[i].split("$")[1];
                 series.strokeWidth = 2;
-                series.tooltip.background.fill=am4core.color("#ffffff00")
+                series.tooltip.getFillFromObject=false;
+                // series.tooltip.background.fill=am4core.color("#ffffff00");
                 series.minBulletDistance = 15;
                 // Drop-shaped tooltips
                 // series.tooltip.background.cornerRadius = 20;
